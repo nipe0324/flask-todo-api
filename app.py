5f264091de85f8a1f4d02f3f56cbf09c5ba7aaac
yanagi <yanagisawa@enigmo.co.jp>
2016-12-31 01:47:21 +0900

    add uri to response

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, abort, request, make_response
+from flask import Flask, jsonify, abort, url_for, request, make_response
 
 app = Flask(__name__)
 
@@ -17,13 +17,22 @@ tasks = [
     }
 ]
 
+def make_public_task(task):
+    new_task = {}
+    for field in task:
+        if field == 'id':
+            new_task['uri'] = url_for('get_task', task_id=task['id'], _external=True)
+        else:
+            new_task[field] = task[field]
+    return new_task
+
 @app.route('/')
 def index():
     return "Hello, world!"
 
 @app.route('/todo/api/v1.0/tasks', methods=['GET'])
 def get_tasks():
-    return jsonify({'tasks': tasks})
+    return jsonify({'tasks': [make_public_task(task) for task in tasks]})
 
 @app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
 def get_task(task_id):
